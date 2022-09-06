c72ad99a8c17db2d540e093bf93e0a8587533273
Ragnarok <s3rg10.martinez.roman@gmail.com>
2022-09-06 18:16:29 +0200

    Step 118

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
+
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+            transform.LookAt(endPosition);
+
+            Debug.Log("Waypoint -> " + waypoint.transform.position);
+            Debug.Log("Enemy -> " + transform.position);
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
+ 
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    
+    [SerializeField] bool isPlaceable;
+
+    void OnMouseDown() 
+    {
+        if (isPlaceable) 
+        {
+            Debug.Log(transform.name);
+        }
+    }
+
+}
