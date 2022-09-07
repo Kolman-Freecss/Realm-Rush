0ec83daf323089fa49e72f7ee01c172b5ae8625b
Ragnarok <s3rg10.martinez.roman@gmail.com>
2022-09-07 20:00:41 +0200

    Step 129

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,26 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
     [SerializeField] int currentBalance;
+    [SerializeField] TextMeshProUGUI displayBalance;
     public int CurrentBalance { get {return currentBalance; } }
 
     void Awake() 
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
+    }
+
+    void UpdateDisplay() 
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void WithDraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         //Lose the game
         if (currentBalance < 0)
--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
-    [SerializeField] int currentHitPoints = 0;
+
+    [Tooltip("Adds amount to maxHitPoints when enemy dies.")]
+    [SerializeField] int difficultyRamp = 1;
+    int currentHitPoints = 0;
     Enemy enemy;
 
     void OnEnable()
@@ -31,6 +35,7 @@ public class EnemyHealth : MonoBehaviour
         if (currentHitPoints <= 0)
         {
             gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
             enemy.RewardGold();
         }
     }
