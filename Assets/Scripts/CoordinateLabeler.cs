a5b537aa538a50b03aee501fb8810871d782ffde
Ragnarok <s3rg10.martinez.roman@gmail.com>
2022-09-07 22:17:48 +0200

    Step 132

--- Assets/Scripts/CoordinateLabeler.cs
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -28,6 +28,7 @@ public class CoordinateLabeler : MonoBehaviour
         {
             DisplayCoordinates();
             UpdateObjectName();
+            label.enabled = true;
         }
         SetLabelColor();
         ToggleLabels();
